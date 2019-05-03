8554f6c40269fe44a45072ce183336b7fee6827e
Miroslav Chabrecek <chabrecek@gmail.com>
2019-05-03 16:44:28 +0200

    Added unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <assert.h>
+#include <memory>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+
+    unique_ptr<int> ls = make_unique<int>(3);
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,1 +1,10 @@
+#include "gmock/gmock.h"
+#include <vector>
 
+using namespace std;
+
+TEST(example, parsing)
+{
+    vector<int> array = {32, 32, 32};
+    EXPECT_THAT(array, testing::ElementsAre(31, 32, 32));
+}
